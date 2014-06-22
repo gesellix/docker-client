e8279dd7d58cff2150cb13859d6cf9c22b31dce6
Tobias Gesellchen <tobias@gesellix.de>
2014-06-22 21:32:29 +0200

    tar file creation (from https://github.com/docker-java/docker-java/blob/master/src/main/java/com/github/dockerjava/client/utils/CompressArchiveUtil.java)

--- src/main/groovy/de/gesellix/docker/client/TarFileBuilder.groovy
+++ src/main/groovy/de/gesellix/docker/client/TarFileBuilder.groovy
@@ -1,43 +1,39 @@
 package de.gesellix.docker.client
 
-import org.apache.commons.compress.archivers.ArchiveOutputStream
-import org.apache.commons.compress.archivers.ArchiveStreamFactory
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry
-import org.apache.commons.io.IOUtils
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream
+import org.apache.commons.io.FileUtils
 
+// from https://github.com/docker-java/docker-java/blob/master/src/main/java/com/github/dockerjava/client/utils/CompressArchiveUtil.java
 class TarFileBuilder {
 
-  def doIt() {
-    /* Output Stream - that will hold the physical TAR file */
-    OutputStream tar_output = new FileOutputStream(new File("tar_ball.tar"))
+  public static File archiveTarFiles(File base, Iterable<File> files, String archiveNameWithOutExtension) throws IOException {
+    File tarFile = new File(FileUtils.getTempDirectoryPath(), archiveNameWithOutExtension + ".tar")
+    tarFile.deleteOnExit()
+    TarArchiveOutputStream tos = new TarArchiveOutputStream(new FileOutputStream(tarFile))
+    try {
+      tos.setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU)
+      for (File file : files) {
+        TarArchiveEntry tarEntry = new TarArchiveEntry(file)
+        tarEntry.setName(relativize(base, file))
 
-    /* Create Archive Output Stream that attaches File Output Stream / and specifies TAR as type of compression */
-    ArchiveOutputStream my_tar_ball = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.TAR, tar_output)
+        tos.putArchiveEntry(tarEntry)
 
-    /* Create Archieve entry - write header information*/
-    File tar_input_file = new File("test_file_1.xml")
-    TarArchiveEntry tar_file = new TarArchiveEntry(tar_input_file)
+        if (!file.isDirectory()) {
+          FileUtils.copyFile(file, tos)
+        }
+        tos.closeArchiveEntry()
+      }
+    }
+    finally {
+      tos.close()
+    }
 
-    /* length of the TAR file needs to be set using setSize method */
-    tar_file.setSize(tar_input_file.length())
-    my_tar_ball.putArchiveEntry(tar_file)
-    IOUtils.copy(new FileInputStream(tar_input_file), my_tar_ball)
+    return tarFile
+  }
 
-    /* Close Archieve entry, write trailer information */
-    my_tar_ball.closeArchiveEntry()
-
-    /* Repeat steps for the next file that needs to be added to the TAR */
-    tar_input_file = new File("test_file_2.xml")
-    tar_file = new TarArchiveEntry(tar_input_file)
-    tar_file.setSize(tar_input_file.length())
-    my_tar_ball.putArchiveEntry(tar_file)
-    IOUtils.copy(new FileInputStream(tar_input_file), my_tar_ball)
-
-    /* Close Archieve entry, write trailer information */
-    my_tar_ball.closeArchiveEntry()
-    my_tar_ball.finish()
-
-    /* Close output stream, our files are zipped */
-    tar_output.close()
+  private static String relativize(File base, File absolute) {
+    String relative = base.toURI().relativize(absolute.toURI()).getPath()
+    return relative
   }
 }
