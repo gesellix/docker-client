18bd73735f94340286153785f8c790c91a252952
Tobias Gesellchen <tobias@gesellix.de>
2014-07-03 20:50:41 +0200

    more tests

--- src/test/groovy/de/gesellix/docker/client/TarFileBuilderTest.groovy
+++ src/test/groovy/de/gesellix/docker/client/TarFileBuilderTest.groovy
@@ -1,5 +1,6 @@
 package de.gesellix.docker.client
 
+import org.apache.commons.io.IOUtils
 import spock.lang.Specification
 
 class TarFileBuilderTest extends Specification {
@@ -12,15 +13,24 @@ class TarFileBuilderTest extends Specification {
     TarFileBuilder.archiveTarFiles()
   }
 
-  def "test copyFile"(){
-    TarFileBuilder.copyFile()
-  }
-
-  def "test relativize"(){
+  def "test relativize"() {
     when:
     def relativized = TarFileBuilder.relativize(new File("./base/dir"), new File("./base/dir/with/sub/dir"))
 
     then:
     relativized == "with/sub/dir"
+  }
+
+  def "test copyFile"() {
+    given:
+    def resource = getClass().getResource('/docker/subdirectory/payload.txt')
+    def inputFile = new File(resource.toURI())
+    def outputStream = new ByteArrayOutputStream()
+
+    when:
+    TarFileBuilder.copyFile(inputFile, outputStream)
+
+    then:
+    new String(outputStream.toByteArray()) == IOUtils.toString(new FileInputStream(inputFile))
   }
 }
