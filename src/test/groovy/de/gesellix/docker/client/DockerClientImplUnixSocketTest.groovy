3365a338c255a70705da8c157ca2b79ec8d86aa8
Tobias Gesellchen <tobias@gesellix.de>
2015-01-09 01:11:17 +0100

    make the DockerUnixSocketTest work on any linux system by optionally running a UnixSocketTestServer

--- src/test/groovy/de/gesellix/docker/client/DockerClientImplUnixSocketTest.groovy
+++ src/test/groovy/de/gesellix/docker/client/DockerClientImplUnixSocketTest.groovy
@@ -1,46 +1,56 @@
 package de.gesellix.docker.client
 
+import org.apache.commons.lang.SystemUtils
 import spock.lang.IgnoreIf
 import spock.lang.Specification
 
-@IgnoreIf({ !new File("/var/run/docker.sock").exists() })
+@IgnoreIf({ !SystemUtils.IS_OS_LINUX })
 class DockerClientImplUnixSocketTest extends Specification {
 
-  File socketFile = new File("/var/run/docker.sock")
-//  File socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "unixsocket-test.sock")
+  File defaultDockerSocket = new File("/var/run/docker.sock")
+  def runDummyDaemon = !defaultDockerSocket.exists()
+  File socketFile = defaultDockerSocket
   DockerClient dockerClient
 
   def setup() {
-    socketFile.createNewFile() || println("$socketFile already exists")
+    if (!defaultDockerSocket.exists()) {
+      socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "unixsocket-dummy.sock")
+      socketFile.deleteOnExit()
+    }
     def unixSocket = "unix://${socketFile.getCanonicalPath()}".toString()
     dockerClient = new DockerClientImpl(dockerHost: unixSocket)
   }
 
   def "info via unix socket"() {
-//    given:
-//    UnixSocketTestServer.runOnce(socketFile, ["the wind": "caught it"].toString().bytes)
+    given:
+    def responseBody = '{"Containers":2,"Images":42}'
+    def expectedResponse = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: application/json",
+        "Job-Name: unix socket test",
+        "Date: Thu, 08 Jan 2015 23:05:55 GMT",
+        "Content-Length: ${responseBody.length()}",
+        "",
+        responseBody
+    ]
+
+    def testserver = null
+    if (runDummyDaemon) {
+      testserver = new UnixSocketTestServer(socketFile)
+      testserver.with {
+        constantResponse = expectedResponse.join("\n")
+      }
+      testserver.runInNewThread()
+    }
+
     when:
     def info = dockerClient.info()
 
     then:
-    info == [
-        Containers        : 0,
-        Debug             : 1,
-        Driver            : "aufs",
-        DriverStatus      : [["Root Dir", "/var/lib/docker/aufs"],
-                             ["Dirs", "252"]],
-        ExecutionDriver   : "native-0.2",
-        IPv4Forwarding    : 1,
-        Images            : 252,
-        IndexServerAddress: "https://index.docker.io/v1/",
-        InitPath          : "/usr/bin/docker",
-        InitSha1          : "",
-        KernelVersion     : "3.13.0-36-generic",
-        MemoryLimit       : 1,
-        NEventsListener   : 0,
-        NFd               : 11,
-        NGoroutines       : 11,
-        OperatingSystem   : "Ubuntu 14.04.1 LTS",
-        SwapLimit         : 0]
+    info.Images >= 0
+    info.Containers >= 0
+
+    cleanup:
+    testserver?.stop()
   }
 }
--- src/test/groovy/de/gesellix/docker/client/UnixSocketTestServer.groovy
+++ src/test/groovy/de/gesellix/docker/client/UnixSocketTestServer.groovy
@@ -2,35 +2,115 @@ package de.gesellix.docker.client
 
 import org.newsclub.net.unix.AFUNIXServerSocket
 import org.newsclub.net.unix.AFUNIXSocketAddress
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 // from https://github.com/gesellix/junixsocket/blob/master/junixsocket/src/demo/org/newsclub/net/unix/demo/SimpleTestServer.java
 class UnixSocketTestServer {
 
-  public static void runOnce(File socketFile, byte[] expectedResponse) throws IOException {
-    AFUNIXServerSocket server = AFUNIXServerSocket.newInstance()
-    server.bind(new AFUNIXSocketAddress(socketFile))
-    System.out.println("server: " + server)
+  Logger logger = LoggerFactory.getLogger(UnixSocketTestServer)
 
+  def greeting = "welcome!"
+  def sendGreeting = false
+  def keepRunning = false
+  def constantResponse = null
+
+  File socketFile
+  def socketThread = null
+
+  public static void main(String[] args) {
+    File socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "unixsocket-server.sock")
+    def server = new UnixSocketTestServer(socketFile)
+    server.with {
+      sendGreeting = true
+      keepRunning = true
+    }
+    server.runInNewThread().join()
+    socketFile.deleteOnExit()
+  }
+
+  UnixSocketTestServer(File socketFile) {
+    this.socketFile = socketFile
+    if (socketFile.exists()) {
+      throw new IllegalStateException("$socketFile already exists - please remove!")
+    }
+  }
+
+  def runInNewThread() throws IOException {
+    socketThread = Thread.start {
+//    Thread.start {
+      AFUNIXServerSocket server = AFUNIXServerSocket.newInstance()
+      server.bind(new AFUNIXSocketAddress(socketFile))
+      println("server: " + server)
+      println("chat with me: 'socat UNIX:${socketFile} -'")
+
+      loop(server)
+    }
+  }
+
+  def loop(AFUNIXServerSocket server) {
+    def sock
+    def is
+    def os
+    def requiresNewConnection = true
     while (!Thread.interrupted()) {
-      System.out.println("Waiting for connection...")
-      Socket sock = server.accept()
-      System.out.println("Connected: " + sock)
+      if (requiresNewConnection) {
+        System.out.println("waiting for a new connection...")
+        sock = server.accept()
+        System.out.println("connected: " + sock)
+        is = sock.getInputStream()
+        os = sock.getOutputStream()
+        requiresNewConnection = false
 
-      InputStream is = sock.getInputStream()
-      OutputStream os = sock.getOutputStream()
-
-      System.out.println("return expectedResponse to client " + os)
-      os.write(expectedResponse)
-      os.flush()
+        if (sendGreeting) {
+          print("return greeting to client...")
+          os.write(greeting.bytes)
+          os.flush()
+        }
+        println("- ok, let's chat!")
+      }
+      assert sock && is && os
 
       byte[] buf = new byte[128]
-      int read = is.read(buf)
-      System.out.println("Client's response: " + new String(buf, 0, read))
+      int read
+      try {
+        read = is.read(buf)
+      }
+      catch (Exception e) {
+        logger.warn("got an error reading bytes from the InputStream", e)
+        return
+      }
+      if (read == -1) {
+        println("EndOfStream - closing connection...")
+        requiresNewConnection = true
 
-      os.close()
-      is.close()
+        closeAll(os, is, sock)
+      }
+      else if (read >= 0) {
+        def clientMessage = new String(buf, 0, read)
+        print("> $clientMessage")
 
-      sock.close()
+        def response = constantResponse ?: "yo $clientMessage"
+        print("< $response")
+        os.write(response.bytes)
+        os.flush()
+
+        if (!keepRunning) {
+          closeAll(os, is, sock)
+        }
+      }
     }
+  }
+
+  def closeAll(os, is, sock) {
+    os.close()
+    is.close()
+    sock.close()
+  }
+
+  def stop() {
+    Thread moribund = socketThread;
+    socketThread = null;
+    moribund?.interrupt();
   }
 }
