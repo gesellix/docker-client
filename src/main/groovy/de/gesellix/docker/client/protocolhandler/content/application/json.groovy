5f3148092add56c8d974f7d4b8988803ed56bd2c
Tobias Gesellchen <tobias@gesellix.de>
2015-03-22 22:00:42 +0100

    apply fix from https://github.com/groovy/groovy-core/pull/440 for Groovy2.3.x compatibility

--- src/main/groovy/de/gesellix/docker/client/protocolhandler/content/application/json.groovy
+++ src/main/groovy/de/gesellix/docker/client/protocolhandler/content/application/json.groovy
@@ -1,6 +1,8 @@
 package de.gesellix.docker.client.protocolhandler.content.application
 
 import groovy.json.JsonSlurper
+import groovy.json.internal.Exceptions
+import groovy.json.internal.FastStringUtils
 import org.apache.commons.io.IOUtils
 
 import java.util.regex.Pattern
@@ -12,7 +14,29 @@ class json extends ContentHandler {
   Pattern multipleChunks = Pattern.compile(".*${chunkDelimiter}.*", Pattern.DOTALL)
 
   json() {
-    jsonSlurper = new JsonSlurper()
+    jsonSlurper = new JsonSlurper() {
+
+      // due to https://github.com/groovy/groovy-core/pull/440
+      // not being available in Groovy < 2.4.x, and the docker-client
+      // library being used in the gradle-docker-plugin, and
+      // Gradle <= 2.4 not allowing us to use Groovy 2.4.x,
+      // we apply the actual fix here.
+      // I love open source :-]
+
+      public Object parse(String jsonString) {
+        return parse(FastStringUtils.toCharArray(jsonString));
+      }
+
+      @Override
+      public Object parse(byte[] bytes, String charset) {
+        try {
+          return parse(new String(bytes, charset));
+        }
+        catch (UnsupportedEncodingException e) {
+          return Exceptions.handle(Object.class, e);
+        }
+      }
+    }
   }
 
   @Override
