1806698480be50abeb4b2608de4d2bc07da39caa
Tobias Gesellchen <tobias@gesellix.de>
2016-04-11 23:42:57 +0200

    copy & paste is not enough -.-

--- src/test/groovy/sun/net/www/protocol/unix/HttpOverNamedPipeIntegrationTest.groovy
+++ src/test/groovy/sun/net/www/protocol/unix/HttpOverNamedPipeIntegrationTest.groovy
@@ -3,30 +3,19 @@ package sun.net.www.protocol.unix
 import de.gesellix.docker.client.DockerConfig
 import de.gesellix.docker.client.HttpClient
 import org.apache.commons.lang.SystemUtils
-import spock.lang.Ignore
 import spock.lang.Requires
 import spock.lang.Specification
 
-@Ignore
+@Requires({ SystemUtils.IS_OS_WINDOWS })
 class HttpOverNamedPipeIntegrationTest extends Specification {
 
-    File defaultDockerSocket = new File("/var/run/docker.sock")
-    def runDummyDaemon = !defaultDockerSocket.exists()
-    File socketFile = defaultDockerSocket
-    HttpClient httpClient
-
-    def setup() {
-        if (true || !defaultDockerSocket.exists()) {
-            runDummyDaemon = true
-            socketFile = new File(new File(System.getProperty("java.io.tmpdir")), "unixsocket-dummy.sock")
-            socketFile.deleteOnExit()
-        }
-        def unixSocket = "unix://${socketFile.getCanonicalPath()}".toString()
-        httpClient = new HttpClient(config: new DockerConfig(dockerHost: unixSocket))
-    }
-
-    def "info via unix socket"() {
+    def "info via named pipe"() {
         given:
+        File namedPipeFile = File.createTempFile("named-pipe", null)
+        namedPipeFile.deleteOnExit()
+        def namedPipe = "npipe://${namedPipeFile.getCanonicalPath()}".toString()
+        HttpClient httpClient = new HttpClient(config: new DockerConfig(dockerHost: namedPipe))
+
         def responseBody = '{"a-key":42,"another-key":4711}'
         def expectedResponse = [
                 "HTTP/1.1 200 OK",
@@ -38,14 +27,10 @@ class HttpOverNamedPipeIntegrationTest extends Specification {
                 responseBody
         ]
 
-        def testserver = null
-        if (runDummyDaemon) {
-            testserver = new UnixSocketTestServer(socketFile)
-            testserver.with {
-                constantResponse = expectedResponse.join("\n")
-            }
-            testserver.runInNewThread()
+        namedPipeFile.withWriter { writer ->
+            writer.append(expectedResponse.join("\n"))
         }
+        println namedPipeFile.text
 
         when:
         def ping = httpClient.get([path: "/_ping"])
@@ -54,6 +39,9 @@ class HttpOverNamedPipeIntegrationTest extends Specification {
         ping.content == ["a-key": 42, "another-key": 4711]
 
         cleanup:
-        testserver?.stop()
+        try {
+            namedPipeFile.delete()
+        } catch (Exception ignore) {
+        }
     }
 }
