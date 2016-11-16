35614d3f1ef220f758bee4391e2daa7568e04c45
Tobias Gesellchen <tobias@gesellix.de>
2016-11-16 09:46:22 +0100

    use windows native container compatible images for tests

--- client/src/integrationTest/groovy/de/gesellix/docker/client/TestimageConstants.groovy
+++ client/src/integrationTest/groovy/de/gesellix/docker/client/TestimageConstants.groovy
@@ -13,6 +13,6 @@ class TestimageConstants {
         imageRepo = "gesellix/docker-client-testimage"
         imageTag = LocalDocker.isNativeWindows() ? "os-windows" : "os-linux"
         imageName = "$imageRepo:$imageTag"
-        imageDigest = LocalDocker.isNamedPipe() ? "todo" : "sha256:a766bfd819b1116a6172c2a7a11a9e90f25222c00bdd740a5ad2575b000a7339"
+        imageDigest = LocalDocker.isNamedPipe() ? "sha256:c7e830fdce2919ea4c8f3b6461e621bfd02f5e0d50b214cfd5317f49103d0b30" : "sha256:a766bfd819b1116a6172c2a7a11a9e90f25222c00bdd740a5ad2575b000a7339"
     }
 }
