e23d551c28651f24ea3d90cb9fead92deb01e8b2
Tobias Gesellchen <tobias@gesellix.de>
2016-12-30 11:24:54 +0100

    try to be faster than the docker hub

--- integration-test/src/test/groovy/de/gesellix/docker/client/TestimageConstants.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/client/TestimageConstants.groovy
@@ -22,7 +22,7 @@ class TestimageConstants {
         if (LocalDocker.isNamedPipe()) {
             imageDigest = "sha256:c7e830fdce2919ea4c8f3b6461e621bfd02f5e0d50b214cfd5317f49103d0b30"
         } else {
-            imageDigest = "sha256:c49c8696daa341d15e417928b1036c2a7459d7d9e9010333cc8c560558f18891"
+            imageDigest = "sha256:029b3e2b03aa60b164b101e8c8d9ba63956c983ab33f1e51c25aa039d9c759f1"
         }
     }
 }
