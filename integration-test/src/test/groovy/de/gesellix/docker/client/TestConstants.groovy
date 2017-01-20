4eca3523e54772998a2cc328f09967a744028cf9
Tobias Gesellchen <tobias@gesellix.de>
2017-01-20 20:39:04 +0100

    fix integration test on Windows with Linux containers

--- integration-test/src/test/groovy/de/gesellix/docker/client/TestConstants.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/client/TestConstants.groovy
@@ -21,7 +21,7 @@ class TestConstants {
         }
         imageName = "$imageRepo:$imageTag"
 
-        if (LocalDocker.isNamedPipe()) {
+        if (LocalDocker.isNativeWindows()) {
             imageDigest = "sha256:c7e830fdce2919ea4c8f3b6461e621bfd02f5e0d50b214cfd5317f49103d0b30"
         } else {
             imageDigest = "sha256:0ce18ad10d281bef97fe2333a9bdcc2dbf84b5302f66d796fed73aac675320db"
