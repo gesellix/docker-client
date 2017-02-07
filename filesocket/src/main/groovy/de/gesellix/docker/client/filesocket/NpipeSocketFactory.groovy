564364f88e32e286ed8314efc3263d66b601afa6
Tobias Gesellchen <tobias@gesellix.de>
2017-02-07 22:09:52 +0100

    move :filesocket to its dedicated repo at https://github.com/docker-client/docker-filesocket

--- filesocket/src/main/groovy/de/gesellix/docker/client/filesocket/NpipeSocketFactory.groovy
+++
@@ -1,9 +0,0 @@
-package de.gesellix.docker.client.filesocket
-
-class NpipeSocketFactory extends FileSocketFactory {
-
-    @Override
-    Socket createSocket() throws IOException {
-        return new NamedPipeSocket()
-    }
-}
