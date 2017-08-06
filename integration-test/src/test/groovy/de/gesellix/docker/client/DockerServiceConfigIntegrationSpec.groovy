545183a1dfe064d60f8b1d39bc45e279a6605a86
Tobias Gesellchen <tobias@gesellix.de>
2017-08-06 22:43:07 +0200

    add integration tests for service configs and secrets.

---
+++ integration-test/src/test/groovy/de/gesellix/docker/client/DockerServiceConfigIntegrationSpec.groovy
@@ -0,0 +1,62 @@
+package de.gesellix.docker.client
+
+import groovy.util.logging.Slf4j
+import spock.lang.Requires
+import spock.lang.Specification
+
+@Slf4j
+@Requires({ LocalDocker.available() && LocalDocker.supportsSwarmMode() })
+class DockerServiceConfigIntegrationSpec extends Specification {
+
+    static DockerClient dockerClient
+
+    def setupSpec() {
+        dockerClient = new DockerClientImpl()
+        performSilently { dockerClient.leaveSwarm([force: true]) }
+    }
+
+    def cleanup() {
+        Thread.sleep(1000)
+    }
+
+    def ping() {
+        when:
+        def ping = dockerClient.ping()
+
+        then:
+        ping.status.code == 200
+        ping.content == "OK"
+    }
+
+    def "expect inactive swarm"() {
+        expect:
+        dockerClient.info().content.Swarm.LocalNodeState == "inactive"
+    }
+
+    def "create, list, and remove a config"() {
+        given:
+        dockerClient.initSwarm()
+
+        when:
+        dockerClient.createConfig("test-config", "some-fancy-stuff".bytes)
+
+        then:
+        def configs = dockerClient.configs([filters: [name: ["test-config"]]]).content
+        configs.size() == 1
+        Map testConfig = configs.first()
+        testConfig.Spec.Name == "test-config"
+        Base64.decoder.decode(testConfig.Spec.Data as String) == "some-fancy-stuff".bytes
+
+        cleanup:
+        performSilently { dockerClient.rmConfig("test-config") }
+        performSilently { dockerClient.leaveSwarm([force: true]) }
+    }
+
+    def performSilently(Closure action) {
+        try {
+            action()
+        }
+        catch (Exception ignored) {
+        }
+    }
+}
---
+++ integration-test/src/test/groovy/de/gesellix/docker/client/DockerServiceSecretIntegrationSpec.groovy
@@ -0,0 +1,61 @@
+package de.gesellix.docker.client
+
+import groovy.util.logging.Slf4j
+import spock.lang.Requires
+import spock.lang.Specification
+
+@Slf4j
+@Requires({ LocalDocker.available() && LocalDocker.supportsSwarmMode() })
+class DockerServiceSecretIntegrationSpec extends Specification {
+
+    static DockerClient dockerClient
+
+    def setupSpec() {
+        dockerClient = new DockerClientImpl()
+        performSilently { dockerClient.leaveSwarm([force: true]) }
+    }
+
+    def cleanup() {
+        Thread.sleep(1000)
+    }
+
+    def ping() {
+        when:
+        def ping = dockerClient.ping()
+
+        then:
+        ping.status.code == 200
+        ping.content == "OK"
+    }
+
+    def "expect inactive swarm"() {
+        expect:
+        dockerClient.info().content.Swarm.LocalNodeState == "inactive"
+    }
+
+    def "create, list, and remove a secret"() {
+        given:
+        dockerClient.initSwarm()
+
+        when:
+        dockerClient.createSecret("test-secret", "some-secret-stuff".bytes)
+
+        then:
+        def secrets = dockerClient.secrets([filters: [names: ["test-secret"]]]).content
+        secrets.size() == 1
+        Map testSecret = secrets.first()
+        testSecret.Spec.Name == "test-secret"
+
+        cleanup:
+        performSilently { dockerClient.rmSecret("test-secret") }
+        performSilently { dockerClient.leaveSwarm([force: true]) }
+    }
+
+    def performSilently(Closure action) {
+        try {
+            action()
+        }
+        catch (Exception ignored) {
+        }
+    }
+}
