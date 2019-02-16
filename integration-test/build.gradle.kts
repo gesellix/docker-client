77d9dbd9220742622ac2d747260e29b114a62f1f
Tobias Gesellchen <tobias@gesellix.de>
2019-02-16 19:47:29 +0100

    work around https://github.com/kohlschutter/junixsocket/issues/59 update dependencies

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -20,11 +20,11 @@ repositories {
 dependencies {
     compile(project(":client"))
 
-    testCompile("net.jodah:failsafe:1.1.1")
+    testCompile("net.jodah:failsafe:2.0.1")
 
     runtime("ch.qos.logback:logback-classic:1.2.3")
 
-    testCompile("de.gesellix:testutil:2018-12-29T16-12-32")
+    testCompile("de.gesellix:testutil:2019-02-16T17-54-28")
     testCompile("org.spockframework:spock-core:1.2-groovy-2.5")
     testCompile("cglib:cglib-nodep:3.2.10")
     testCompile("org.apache.commons:commons-lang3:3.8.1")
