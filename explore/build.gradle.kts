fd38fd2ad72562b8f6738703fec641bdee21a38f
Tobias Gesellchen <tobias@gesellix.de>
2019-03-17 22:26:40 +0100

    update dependencies

--- explore/build.gradle.kts
+++ explore/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     compile(project(":client"))
     runtime("ch.qos.logback:logback-classic:1.2.3")
 
-    testCompile("org.spockframework:spock-core:1.2-groovy-2.5")
+    testCompile("org.spockframework:spock-core:1.3-groovy-2.5")
     testCompile("cglib:cglib-nodep:3.2.10")
     testCompile("ch.qos.logback:logback-classic:1.2.3")
 }
