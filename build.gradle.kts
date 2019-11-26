428cefed4ef94234e79795258803bbfd75127779
Tobias Gesellchen <tobias@gesellix.de>
2019-11-26 01:10:13 +0100

    Enable GitHub Package Registry publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,10 @@ buildscript {
 }
 
 plugins {
-    id("com.github.ben-manes.versions") version "0.21.0"
-    id("net.ossindex.audit") version "0.4.11"
+    id("com.github.ben-manes.versions") version "0.27.0"
     id("com.jfrog.bintray") version "1.8.4" apply false
+    id("net.ossindex.audit") version "0.4.11"
+    id("io.freefair.github.package-registry-maven-publish") version "4.1.5" // apply false
 }
 
 val dependencyVersions = listOf(
@@ -35,6 +36,14 @@ subprojects {
             force(dependencyVersions)
         }
     }
+}
+
+fun findProperty(s: String) = project.findProperty(s) as String?
+
+rootProject.github {
+    slug.set("${project.property("github.package-registry.owner")}/${project.property("github.package-registry.repository")}")
+    username.set(System.getenv("GITHUB_ACTOR") ?: findProperty("github.package-registry.username"))
+    token.set(System.getenv("GITHUB_TOKEN") ?: findProperty("github.package-registry.password"))
 }
 
 tasks {
