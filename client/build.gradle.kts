3db34542b24e508d26a011c78b6bb67f487d6947
Tobias Gesellchen <tobias@gesellix.de>
2022-02-01 11:55:07 +0100

    bump docker-remote-api-* dependencies

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
 
   // TODO consider changing this from api to implementation.
   // The change would require to move api.core client classes like `ClientException` to another module.
-  api("de.gesellix:docker-remote-api-client:2022-01-31T20-50-00")
-  api("de.gesellix:docker-remote-api-model-1-41:2021-11-21T21-26-00")
+  api("de.gesellix:docker-remote-api-client:2022-02-01T11-40-00")
+  api("de.gesellix:docker-remote-api-model-1-41:2022-02-01T11-07-00")
   api("de.gesellix:docker-engine:2021-12-18T13-34-00")
   api("de.gesellix:docker-compose:2021-12-18T12-41-00")
 
