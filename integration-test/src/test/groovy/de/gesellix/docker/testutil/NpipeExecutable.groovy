9fd22821e1c23efaf726f566f08c62b9928ec33c
Tobias Gesellchen <tobias@gesellix.de>
2022-04-19 16:20:49 +0200

    Use the multi-platform gesellix/npipe image

--- integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
@@ -1,14 +1,13 @@
 package de.gesellix.docker.testutil
 
 import de.gesellix.docker.client.DockerClientImpl
-import de.gesellix.docker.client.LocalDocker
 import de.gesellix.docker.client.container.ArchiveUtil
 import de.gesellix.docker.remote.api.ContainerCreateRequest
 
 class NpipeExecutable {
 
   File createNpipeExe(DockerClientImpl docker) {
-    String repository = LocalDocker.isNativeWindows() ? "gesellix/npipe:windows" : "gesellix/npipe:latest"
+    String repository = "gesellix/npipe:2022-04-19T16-14-00"
     docker.createContainer(new ContainerCreateRequest().tap { image = repository }, "npipe")
     InputStream archive = null
     try {
