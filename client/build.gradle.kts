dc1f3155c2a91f967e5b123662d9fac3e6a93c27
Tobias Gesellchen <tobias@gesellix.de>
2022-10-03 21:16:24 +0200

    Bump logback-classic to 1.3.3

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -109,7 +109,7 @@ dependencies {
   implementation("com.google.re2j:re2j:1.7")
 
   implementation("org.slf4j:slf4j-api:2.0.3")
-  testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.3.1")
+  testImplementation("ch.qos.logback:logback-classic:[1.2,2)!!1.3.3")
 
   implementation("com.squareup.okio:okio-jvm:3.2.0")
   api("com.squareup.okhttp3:okhttp:4.10.0")
