7290626fe666d66ca7fe159c8d527b0cbdd99f80
Tobias Gesellchen <tobias@gesellix.de>
2023-02-19 11:22:33 +0100

    configure toolchains resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
 rootProject.name = "docker-client"
 include("client", "integration-test", "explore")
+
+// https://docs.gradle.org/8.0.1/userguide/toolchains.html#sub:download_repositories
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+}
