ce0515e6d438d799f4caf0f01c50a9fa3556b27f
Tobias Gesellchen <tobias@gesellix.de>
2023-03-27 23:24:39 +0200

    Prefer logback-classic 1.3.6

--- explore/build.gradle.kts
+++ explore/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   testImplementation("org.apache.commons:commons-compress:1.23.0")
 
   implementation("org.slf4j:slf4j-api:2.0.7")
-  runtimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.3.5")
+  runtimeOnly("ch.qos.logback:logback-classic:[1.2,2)!!1.3.6")
 
   testImplementation("org.spockframework:spock-core:2.3-groovy-3.0")
   testRuntimeOnly("net.bytebuddy:byte-buddy:1.14.2")
