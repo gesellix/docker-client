c4ec2db41740960495636039df273886e3e18387
Tobias Gesellchen <tobias@gesellix.de>
2023-05-08 19:36:00 +0200

    Don't call "/info" API endpoint to get default registry (cleanup)

--- client/src/main/java/de/gesellix/docker/client/authentication/RegistryElection.java
+++ client/src/main/java/de/gesellix/docker/client/authentication/RegistryElection.java
@@ -3,20 +3,12 @@ package de.gesellix.docker.client.authentication;
 import de.gesellix.docker.authentication.AuthConfig;
 import de.gesellix.docker.authentication.AuthConfigReader;
 import de.gesellix.docker.engine.DockerEnv;
-import de.gesellix.docker.remote.api.SystemInfo;
-import de.gesellix.docker.remote.api.client.SystemApi;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RegistryElection {
 
-  private static final Logger log = LoggerFactory.getLogger(RegistryElection.class);
-
-  private final SystemApi systemApi;
   private final AuthConfigReader authConfigReader;
 
-  public RegistryElection(SystemApi systemApi, AuthConfigReader authConfigReader) {
-    this.systemApi = systemApi;
+  public RegistryElection(AuthConfigReader authConfigReader) {
     this.authConfigReader = authConfigReader;
   }
 
@@ -36,24 +28,5 @@ public class RegistryElection {
   public String electAuthServer() {
     final String defaultServerAddress = new DockerEnv().getIndexUrl_v1();
     return defaultServerAddress;
-
-    // deprecated/obsolete: see https://github.com/docker/cli/pull/2819 for details
-    //
-    // The daemon `/info` endpoint informs us of the default registry being
-    // used. This is essential in cross-platforms environment, where for
-    // example a Linux client might be interacting with a Windows daemon, hence
-    // the default registry URL might be Windows specific.
-//    try {
-//      SystemInfo info = systemApi.systemInfo();
-//      if (info == null || info.getIndexServerAddress() == null || info.getIndexServerAddress().isEmpty()) {
-//        log.warn("Empty registry endpoint from daemon. Using system default: " + defaultServerAddress);
-//        return defaultServerAddress;
-//      } else {
-//        return info.getIndexServerAddress();
-//      }
-//    } catch (Exception e) {
-//      log.warn("Failed to get default registry endpoint from daemon. Using system default: " + defaultServerAddress, e);
-//      return defaultServerAddress;
-//    }
   }
 }
