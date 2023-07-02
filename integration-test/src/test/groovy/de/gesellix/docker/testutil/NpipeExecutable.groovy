e393886b6786bce7db155bcbf29e838871ba5af8
Tobias Gesellchen <tobias@gesellix.de>
2023-07-02 20:43:13 +0200

    Bump gesellix/npipe test image

--- integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
@@ -7,7 +7,7 @@ import de.gesellix.docker.remote.api.ContainerCreateRequest
 class NpipeExecutable {
 
   File createNpipeExe(DockerClientImpl docker) {
-    String repository = "gesellix/npipe:2022-07-31T14-30-00"
+    String repository = "gesellix/npipe:2023-07-02T11-43-00"
     docker.createContainer(new ContainerCreateRequest().tap { image = repository }, "npipe")
     InputStream archive = docker.getArchive("npipe", "/npipe.exe").content
     def npipeExe = new File("npipe.exe")
