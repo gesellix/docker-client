fb9f9f373b0b32bfeaa16d4f3777366cc90b46d0
Tobias Gesellchen <tobias@gesellix.de>
2024-02-06 22:21:17 +0100

    Prefer slf4j 2.0.12

--- explore/build.gradle.kts
+++ explore/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation("org.slf4j:slf4j-api") {
       version {
         strictly("[1.7,3)")
-        prefer("2.0.11")
+        prefer("2.0.12")
       }
     }
     implementation("com.squareup.okio:okio") {
