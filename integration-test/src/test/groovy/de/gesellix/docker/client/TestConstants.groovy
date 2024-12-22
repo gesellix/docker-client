8517a1d6103932f48131a73e21493156cdf856b0
Tobias Gesellchen <tobias@gesellix.de>
2024-12-22 17:36:11 +0100

    Bump echo-server image

--- integration-test/src/test/groovy/de/gesellix/docker/client/TestConstants.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/client/TestConstants.groovy
@@ -15,27 +15,28 @@ class TestConstants {
   static TestConstants CONSTANTS = new TestConstants()
 
   TestConstants() {
-    // docker inspect --format "{{ json .Created }}, Id: {{ json .Id }}, Digests: {{ json .RepoDigests }}" gesellix/echo-server:2024-09-15T19-35-00
+    // docker inspect --format "{{ json .Created }}, Id: {{ json .Id }}, Digests: {{ json .RepoDigests }}" gesellix/echo-server:2025-07-27T22-12-00
     if (LocalDocker.isNativeWindows()) {
-      imageDigest = "gesellix/echo-server@sha256:de454e47bcd8ff5247545ce3c886c0af93be127b771fcd3bfc3651744252e335"
-      imageId = "sha256:662e836a255b25d8c9060c325953027a5296ab8bf689794467361449ead0d1b8"
-      imageCreated = 1726421760
+      imageDigest = "gesellix/echo-server@sha256:e07c7a757334d40c56035c74d4e46a6c4b0295fe6cb8fd22c0db81b5f8670365"
+      imageId = "sha256:8d5dfeabe0e079a04ceac6be266dbc13230ec1e018ac04ff1f244f62b465e49e"
+      imageCreated = 1753647324
       volumeTarget = "C:/my-volume"
     } else {
-      imageDigest = "gesellix/echo-server@sha256:de454e47bcd8ff5247545ce3c886c0af93be127b771fcd3bfc3651744252e335"
+      imageDigest = "gesellix/echo-server@sha256:e07c7a757334d40c56035c74d4e46a6c4b0295fe6cb8fd22c0db81b5f8670365"
       // this one works on GitHub
-      imageId = "sha256:eba6f6644b5255fc5b6ab4dcd4b63495ae2d438723e7da4795ed52dc6d06856d"
+      imageId = "sha256:c59e7878cd83c4bc1703f8be1e440c00eed24ccb0e196dae234abea2fb39277f"
+      imageCreated = 1753647218
       // this one works for containerd
-//      imageId = "sha256:de454e47bcd8ff5247545ce3c886c0af93be127b771fcd3bfc3651744252e335"
-      imageCreated = 1726421659
+//      imageId = "sha256:e07c7a757334d40c56035c74d4e46a6c4b0295fe6cb8fd22c0db81b5f8670365"
+//      imageCreated = 1753647508
       volumeTarget = "/my-volume"
     }
     imageRepo = "gesellix/echo-server"
-    imageTag = "2024-09-15T19-35-00"
+    imageTag = "2025-07-27T22-12-00"
     imageName = "$imageRepo:$imageTag"
 
     versionDetails = [
-        ApiVersion   : { it in ["1.43", "1.44", "1.45", "1.46", "1.47", "1.48"] },
+        ApiVersion   : { it in ["1.43", "1.44", "1.45", "1.46", "1.47", "1.48", "1.49", "1.50", "1.51"] },
         Arch         : { it in ["amd64", "arm64"] },
         BuildTime    : { it =~ "\\d{4}-\\d{2}-\\d{2}T\\w+" },
         GitCommit    : { it =~ "\\w{6,}" },
