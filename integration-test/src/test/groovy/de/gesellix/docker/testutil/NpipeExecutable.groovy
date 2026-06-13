bc89967f302c638c74e1657b9328f53d6bf6edec
Tobias Gesellchen <tobias@gesellix.de>
2026-06-13 21:35:30 +0200

    Update npipe to 2.0.0-202606131520

--- integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
+++ integration-test/src/test/groovy/de/gesellix/docker/testutil/NpipeExecutable.groovy
@@ -7,7 +7,7 @@ import de.gesellix.docker.remote.api.ContainerCreateRequest
 class NpipeExecutable {
 
   File createNpipeExe(DockerClientImpl docker) {
-    String repository = "gesellix/npipe:2024-09-15T22-35-00"
+    String repository = "gesellix/npipe:2.0.0-202606131520"
     docker.createContainer(new ContainerCreateRequest().tap { image = repository }, "npipe")
     InputStream archive = docker.getArchive("npipe", "/npipe.exe").content
     def npipeExe = new File("npipe.exe")
